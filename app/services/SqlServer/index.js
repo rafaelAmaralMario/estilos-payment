3d37fcc7c8bf1e79856edfab87ddc62a91deeb0a
Rafael Mario <rafaelamaralmario@gmail.com>
2022-01-13 11:54:19 -0300

    ádd new sql lib

--- app/services/SqlServer/index.js
+++ app/services/SqlServer/index.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 const {getEnvironmentVariable} = require('../../config');
 const {LogFactory} = require('../../../logger')
+const sql = require('mssql')
 
 async function connectToDataBase() {
     const logger = LogFactory.logger();
@@ -28,12 +29,35 @@ async function connectToDataBase() {
 
 }
 
-async function getSQLTarifario(tarjeta) {
+
+
+  
+  async function getSQLTarifario(tarjeta) {
+    const dbName = getEnvironmentVariable("DB_NAME")
+    const dbUser = getEnvironmentVariable("DB_USER")
+    const dbPass = getEnvironmentVariable("DB_PASS")
+    const dbHost = getEnvironmentVariable("DB_HOST")
 
     try {
-        const sequelize = await connectToDataBase();
-
-        const results = await sequelize.query(`EXEC dbo.sp_web_consultaTarifario ${tarjeta}`);
+        const sqlConfig = {
+            user: dbUser,
+            password: dbPass,
+            database: dbName,
+            server: dbHost,
+            pool: {
+              max: 10,
+              min: 0,
+              idleTimeoutMillis: 30000
+            },
+            options: {
+              encrypt: true, // for azure
+              trustServerCertificate: false // change to true for local dev / self-signed certs
+            }
+          }
+            logger.debug(`Connection with Database`);
+           // make sure that any items are correctly URL encoded in the connection string
+            await sql.connect(sqlConfig)
+            const results = await sql.query`EXEC dbo.sp_web_consultaTarifario ${tarjeta}`
 
       return results;
     } catch (error) {
@@ -41,6 +65,8 @@ async function getSQLTarifario(tarjeta) {
     }
     
 }
+
+
 
 module.exports = {
     getSQLTarifario
