2080bf03dcdaeab689dc2535110a781ed2cf6112
Rafael Mario <rafaelamaralmario@gmail.com>
2022-01-26 13:37:37 -0300

    updating heroku

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(function(req, res, next) {
   if (!res.locals) {
     res.locals = {};
   }
+
+ // Website you wish to allow to connect
+ res.setHeader('Access-Control-Allow-Origin', '*');
+
+ // Request methods you wish to allow
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   //http://expressjs.com/en/api.html#res.locals
   //use res.locals to pass object between main and sub apps
   res.locals.logger = logger;
@@ -22,11 +28,6 @@ app.use(function(req, res, next) {
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 app.use("/ccstorex/custom", require("./app/index"));
- // Website you wish to allow to connect
- res.setHeader('Access-Control-Allow-Origin', '*');
-
- // Request methods you wish to allow
- res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 // Read port from command line, config, or default
 var port = (process.env.PORT || process.argv[2] || (process.env.npm_package_config_port || 3000));
 
